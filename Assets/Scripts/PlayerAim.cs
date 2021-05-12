6be1bcc1f234131bffe9e5217977487b25799997
Pedro Ramos <pedrotramos97@gmail.com>
2021-05-12 16:30:10 -0300

    first commit

---
+++ Assets/Scripts/PlayerAim.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAim : MonoBehaviour
+{
+    GameObject aimTransform;
+    Vector2 aimDirection;
+
+    void Awake()
+    {
+        aimTransform = GameObject.FindGameObjectWithTag("Aim");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = GetMouseWorldPosition();
+        Vector2 pos = new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y);
+        aimDirection = pos.normalized;
+        float angle = (Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg) - 90f;
+        float xVal = transform.position.x + (aimDirection.x * 0.7f);
+        float yVal = transform.position.y + (aimDirection.y * 0.7f);
+        aimTransform.transform.position = new Vector2(xVal, yVal);
+        aimTransform.transform.eulerAngles = new Vector3(0f, 0f, angle);
+    }
+
+    Vector3 GetMouseWorldPosition()
+    {
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        return pos;
+    }
+
+    public Vector2 GetAimDirection()
+    {
+        return aimDirection;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public PlayerAim aim;
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector2 direction = aim.GetAimDirection();
+            float force = 400f;
+            rb.AddForce(direction * force);
+        }
+    }
+}
